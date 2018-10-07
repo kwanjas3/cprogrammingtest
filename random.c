90d1eb2a674372924e23c244b772117336e0f312
kwanjas3 <kwanjas0412@gmail.com>
2018-10-07 16:16:50 -0400

    i added some changes

--- random.c
+++ random.c
@@ -1,40 +1,42 @@
-include <stdio.h>
+include<stdio.h>
 
-
-
-for (int i = 0; i < 10; i++){
+    for (int i = 0; i < 10; i++)
+{
     printf("stuff in array = %lf", dayhighdaylow[i]);
 }
 
+hello world
 
-
-int array[0] = 0;
+    int array[0] = 0;
 // I know how big the loops
 
 int flag;
 int uptodays = 0;
 int i = 0;
-while(flag!=-1){
+while (flag != -1)
+{
 
-scanf("%d", uptodays);
-if(uptodays < 5 && uptodays > 0){
-    double avg = 0;
+    scanf("%d", uptodays);
+    if (uptodays < 5 && uptodays > 0)
+    {
+        double avg = 0;
 
-    ((float)(x + y)) / 2
+        ((float)(x + y)) / 2
 
-    printf("The average temperature up to day 3 is ");
-    printf("%lf", avg)
-}
-if (uptodays is valid or negative){
-    flag = 0;
-}  
-for (int i = 0; i< 4; i++){                            i=0, j0,j1,j2,j3, i1, j0,j1,j2,j3, i2 j0,j1,j2,j3
-    //this doesn't know j
-    for (int j = 0; i < 4; j++){
-        //this knows about i and j
+            printf("The average temperature up to day 3 is ");
+        printf("%lf", avg)
     }
-
-
-
-}
+    if (uptodays is valid or negative)
+    {
+        flag = 0;
+    }
+    for (int i = 0; i < 4; i++)
+    {
+        i = 0, j0, j1, j2, j3, i1, j0, j1, j2, j3, i2 j0, j1, j2, j3
+            //this doesn't know j
+            for (int j = 0; i < 4; j++)
+        {
+            //this knows about i and j
+        }
+    }
 }
